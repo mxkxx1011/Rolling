e481fd9fc48ae7b0d25c0728c5cf1bb07b7a1739
HanjuJung <hjvzx3502@gmail.com>
2024-06-12 02:20:29 +0900

    Add Button, ArrowButton, PlusButton, DeleteButton components and ToggleButton is not complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import ArrowButton from 'components/ArrowButton';
+import Button from './components/Button';
+import PlusButton from 'components/PlusButton';
+import DeleteButton from 'components/DeleteButton';
+
 function App() {
-  return <h2>react app</h2>;
+  return (
+    <>
+      <Button disable={false} type='outlined' size='40' emoji={true}>
+        한주바보
+      </Button>
+      <Button disable={true} type='secondary' size='28' emoji={false}>
+        Disabled
+      </Button>
+      <ArrowButton direction='right' />
+      <PlusButton disable={false} />
+      <DeleteButton disable={false} />
+    </>
+  );
 }
 
+// Button - disable, emoji: true or false / type: primary or secondary or outlined / size: 56 or 40 or 36 or 28
+// ArrowButton - direction: left or right
 export default App;
