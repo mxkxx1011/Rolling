6c800c559b7800ee65b43f73bab1fc294c465294
HanjuJung <166710330+HanjuJung@users.noreply.github.com>
2024-06-12 14:28:28 +0900

    feat: 버튼 컴포넌트 구현 (#9)
    토글 버튼 미구현
    Add Button, ArrowButton, PlusButton, DeleteButton components and ToggleButton is not complete
    Co-authored-by: mxkxx1011 <100355178+mxkxx1011@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,23 @@ function App() {
       <Header />
       <Routes></Routes>
     </Router>
+
   );
 }
 
+// Button - disable, emoji: true or false / type: primary or secondary or outlined / size: 56 or 40 or 36 or 28
+// ArrowButton - direction: left or right
 export default App;
+
+
+// <>
+//       <Button disable={false} type='outlined' size='40' emoji={true}>
+//         한주바보
+//       </Button>
+//       <Button disable={true} type='secondary' size='28' emoji={false}>
+//         Disabled
+//       </Button>
+//       <ArrowButton direction='right' />
+//       <PlusButton disable={false} />
+//       <DeleteButton disable={false} />
+//     </>
