b99bb0f7debec37f39fd6dc0c21f44c371674d02
soyoung <ssh0040233@gmail.com>
2024-06-14 03:49:40 +0900

    feat: 뱃지 컴포넌트 조건부 렌더링 구현

--- src/components/badge/Badge.jsx
+++ src/components/badge/Badge.jsx
@@ -1,12 +1,21 @@
 import './Badge.scss';
 import React from 'react';
 
+const BadgeColor = {
+  지인: 'BeigeBadge',
+  동료: 'PurpleBadge',
+  가족: 'GreenBadge',
+  친구: 'BlueBadge',
+};
+
+//전달받은 BadgeColor에 따라 뱃지의 className이 변경되도록 조건부 렌더링
 function Badge({ children }) {
+  const badgeClassName = `text-badge-container ${BadgeColor[children] || '친구'}`;
   return (
     <>
-      <span className='text-badge-container'>
+      <div className={badgeClassName}>
         <p>{children}</p>
-      </span>
+      </div>
     </>
   );
 }
