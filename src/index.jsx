d1c98e6fd87689a37876bf9864298057a34123ce
soyoung <ssh0040233@gmail.com>
2024-06-14 03:05:53 +0900

    refactor: scss 리팩토링

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from 'App';
 import 'assets/styles/fonts.scss';
 import 'assets/styles/reset.scss';
-import Modal from 'components/modal/Modal';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Modal />
   </React.StrictMode>,
 );
