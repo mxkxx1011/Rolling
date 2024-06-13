e48ca8d0936bd02e75cc9a403b2a84569083dff1
mxkxx <mxkxx@naver.com>
2024-06-13 12:56:59 +0900

    chore: 라우트 경로 수정, 사용할 페이지 모두 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import CardListPage from 'pages/CardListPage';
+import CardMessageDeletePage from 'pages/CardMessageDeletePage';
+import CardMessagePage from 'pages/CardMessagePage';
+import CardMessagePostPage from 'pages/CardMessagePostPage';
 import HomePage from 'pages/home/HomePage';
 import Main from 'pages/Main';
+import NotFoundPage from 'pages/NotFoundPage';
+import PostPage from 'pages/PostPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -8,7 +14,17 @@ function App() {
       <Routes>
         <Route path='/' element={<Main />}>
           <Route index element={<HomePage />} />
+          <Route path='list' element={<CardListPage />} />
+          <Route path='post'>
+            <Route index element={<PostPage />} />
+            <Route path=':id'>
+              <Route index element={<CardMessagePage />} />
+              <Route path='edit' element={<CardMessageDeletePage />} />
+              <Route path='message' element={<CardMessagePostPage />} />
+            </Route>
+          </Route>
         </Route>
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/CardMessagePostPage.jsx
@@ -0,0 +1,5 @@
+function CardMessagePostPage() {
+  return <h2>롤링페이퍼 카드 메시지 작성하는 페이지</h2>;
+}
+
+export default CardMessagePostPage;
