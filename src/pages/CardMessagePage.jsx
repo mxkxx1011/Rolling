3b24d2a8abfb47b7f261b289934e1337617c4408
mxkxx <mxkxx@naver.com>
2024-06-14 13:07:59 +0900

    💩 api 작업 전이라 에러나서 페이지 컴포넌트 주석처리
    pr 올리고 머지 후 수정할 예정

--- src/pages/CardMessagePage.jsx
+++ src/pages/CardMessagePage.jsx
@@ -1,20 +1,29 @@
 import CardList from 'components/card/CardList';
 import Header from 'components/Header';
 import HeaderCardMessage from 'components/header/HeaderCardMessage';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 // post/{id}
 function CardMessagePage() {
+  const [recipient, setRecipient] = useState();
+  const [recipientMessage, setRecipientMessage] = useState();
+
+  useEffect(() => {}, []);
+
   const { id } = useParams();
   return (
     <>
-      <HeaderCardMessage
+      <h2>card message page /post/id </h2>
+      {/* <HeaderCardMessage
         name={name}
         messageCount={messageCount}
         recentMessages={recentMessages}
         reactions={reactions}
       />
-      <CardList recipient={recipient} />
+      <CardList recipient={recipient} /> */}
     </>
   );
 }
