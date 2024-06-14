b4ab2d5dfe475cc5daecfad9dfc85b5cb671e8b0
ouranos0 <ouranos0@naver.com>
2024-06-14 18:03:12 +0900

    feat : 이모지 토글 구현 body : 스타일과 이모지의 기능 관련 수정 중

--- src/components/EmojiToggle.jsx
+++ src/components/EmojiToggle.jsx
@@ -1,38 +1,41 @@
-import { useState } from "react"
-import ArrowDown from "assets/images/ic_arrow_down.svg";
-import ArrowTop from "assets/images/ic_arrow_top.svg";
-import "components/EmojiToggle";
+import { useState } from 'react';
+import ArrowDown from 'assets/images/ic_arrow_down.svg';
+import ArrowTop from 'assets/images/ic_arrow_top.svg';
+import 'components/EmojiToggle.scss';
+
 function EmojiToggle() {
-    // 롤링 페이퍼의 모든 이모지 데이터 받아오는거 추가
-    const [isOpen, setIsOpen] = useState(false);
+  // 롤링 페이퍼의 모든 이모지 데이터 받아오는거 추가
+  const [isOpen, setIsOpen] = useState(false);
 
-    const onToggle = () => {
-        setIsOpen(!isOpen);
-    }
+  const onToggle = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const testemoji = "👌";
-    const testcount = [1, 2, 3, 4];
-    return (
-        <div className="emojilistlayer">
-            <button className="emojitoggle" onClick={onToggle}>
-                <img src={isOpen ? ArrowDown : ArrowTop} alt="이모지토글" className="arrow" />
-            </button>
-            {
-                isOpen && (
-                    <div className="allemojilist">
-                        {/* 반복문으로 이모지 데이터 접근 */}
-                        {testcount.map((a, index) => (
-                            <div className="emoji">
-                                {testemoji}
-                                {a}
-                            </div>
-                        ))
-                        }
-                    </div>
-                )
-            }
+  const testemoji = '👌';
+  const testcount = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  return (
+    <div className='emojilistlayer'>
+      <button className='emojitoggle' onClick={onToggle}>
+        <img
+          src={isOpen ? ArrowTop : ArrowDown}
+          alt='이모지토글'
+          className='arrow'
+        />
+      </button>
+      {isOpen && (
+        <div className='allemojilist'>
+          {/* 이모지 리스트 중복? 처리에 대한 질문있음 배경색도 없는수치임 */}
+          {/* 반복문으로 이모지 데이터 접근 */}
+          {testcount.map((a, index) => (
+            <div className='emoji'>
+              {testemoji}
+              {a}
+            </div>
+          ))}
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
 export default EmojiToggle;
--- src/pages/CardListPage.jsx
+++ src/pages/CardListPage.jsx
@@ -1,19 +1,34 @@
-import { RecipientsAPI } from "data/CallAPI";
-import { useState, useEffect } from "react";
+import { RecipientsMessagesAPI } from 'data/CallAPI';
+import { useState, useEffect } from 'react';
 // import CardList from "components/CardList";
-import TextDropdownField from "components/textfield/TextDropdownField";
-import TextInputField from "components/textfield/TextInputField";
-import EmojiToggle from "components/EmojiToggle";
+import TextDropdownField from 'components/textfield/TextDropdownField';
+import TextInputField from 'components/textfield/TextInputField';
+import EmojiToggle from 'components/EmojiToggle';
 
 function CardListPage() {
   const [cardList, setCardList] = useState([]);
-  const method = "get";
+  const method = 'get';
+  // const [pp, setPp] = useState({});
 
   // setCardList(CardList(method));
-  const test = ["a", "b", "c", "d"] ;
+  const test = ['a', 'b', 'c', 'd'];
+
+  // useEffect(() => {
+  //   setPp(RecipientsMessagesAPI('get', '7720') : "1");
+  // }, []);
+
+  // useEffect(() => {
+  //   // RecipientsMessagesAPI가 비동기 함수라고 가정하고 async/await 사용
+  //   async function fetchData() {
+  //     const result = await RecipientsMessagesAPI('get', '7720');
+  //     setPp(result);
+  //   }
+  //   fetchData();
+  // }, []);
+
   return (
-    <div className="cardlistlayer">
-      <div className="hotcardlist">
+    <div className='cardlistlayer'>
+      <div className='hotcardlist'>
         <EmojiToggle></EmojiToggle>
         <TextDropdownField options={test}></TextDropdownField>
         <TextInputField></TextInputField>
@@ -21,10 +36,8 @@ function CardListPage() {
         {/* 반복문 */}
         {/* <CardList /> */}
       </div>
-      <div className="latelycardlist">
-        {/* <CardList /> */}
-      </div>
-    </div >
+      <div className='latelycardlist'>{/* <CardList /> */}</div>
+    </div>
   );
 }
 
