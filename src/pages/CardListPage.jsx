88c7ca21b904460d534975aedb8d3125dd7c8f24
ouranos0 <ouranos0@naver.com>
2024-06-14 16:41:42 +0900

    style : 이모지 스타일 수정 body : 이모지 스타일 구현 중

--- src/pages/CardListPage.jsx
+++ src/pages/CardListPage.jsx
@@ -2,6 +2,7 @@ import { RecipientsAPI } from "data/CallAPI";
 import { useState, useEffect } from "react";
 // import CardList from "components/CardList";
 import TextDropdownField from "components/textfield/TextDropdownField";
+import TextInputField from "components/textfield/TextInputField";
 import EmojiToggle from "components/EmojiToggle";
 
 function CardListPage() {
@@ -14,6 +15,8 @@ function CardListPage() {
     <div className="cardlistlayer">
       <div className="hotcardlist">
         <EmojiToggle></EmojiToggle>
+        <TextDropdownField options={test}></TextDropdownField>
+        <TextInputField></TextInputField>
         {/* <TextDropdownField options={test}></TextDropdownField> */}
         {/* 반복문 */}
         {/* <CardList /> */}
