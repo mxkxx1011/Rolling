737b22eabc9d24093ed232fd61b1d581e32ff47c
soyoung <ssh0040233@gmail.com>
2024-06-15 03:11:35 +0900

    feat: post 페이지 퍼블리싱 작업(1)

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,5 +1,34 @@
+//import ToggleButton from 'components/ToggleButton';
+import TextInputField from 'components/textinputfield/TextInputField';
+import Button from 'components/Button';
+import './PostPage.scss';
+
 function PostPage() {
-  return <h2>post page</h2>;
+  return (
+    <>
+      <div className='page-container post'>
+        <div>
+          <form className='text-container'>
+            <label>
+              <p className='font-24-bold'>To.</p>
+            </label>
+            <TextInputField>받는 사람 이름을 선택해 주세요</TextInputField>
+          </form>
+        </div>
+        <div className='text-container'>
+          <p className='font-24-bold'>배경화면을 선택해 주세요.</p>
+          <p className='font-16-regular'>
+            컬러를 선택하거나, 이미지를 선택할 수 있습니다.
+          </p>
+        </div>
+        {/* 토글버튼자리 */}
+        {/* 옵션 컴포넌트 자리 */}
+        <Button type='primary' size='40'>
+          생성하기
+        </Button>
+      </div>
+    </>
+  );
 }
 
 export default PostPage;
