b7f3b05c27396426d8c90c9d3a976ee361566465
HanjuJung <166710330+HanjuJung@users.noreply.github.com>
2024-06-18 14:43:49 +0900

    feat: 홈페이지 구현 중 (#35)
    * Feat: 뱃지 컴포넌트, 인풋 컴포넌트 기능 추가 구현 (#1)
    
    1. 인풋 컴포넌트 기능 추가
    * input width 반응형 처리
    * placeholder 값을 children props로 받아오도록 작업
    
    2. 뱃지 컴포넌트 조건부 렌더링
    * [지인, 동료, 가족, 친구] 값에 따라서 뱃지의 background-color와 font color가 변경되도록 조건부 렌더링 적용
    * 이전에 작성한 모달 scss파일 삭제 -> assets/styles/CardModal.scss에 구현되어있기 때문
    * 토스트, 뱃지 컴포넌트 scss 파일 리팩토링 (코드리뷰를 바탕으로)
    
    * feat: 헤더 조건부 렌더링 구현 (#15)
    
    * feat: 헤더 조건부 렌더링 구현
    
    - 모바일이면서 포스트 페이지일 때 헤더 안보이기
    - 포스트 페이지가 아닐 때 롤링 페이퍼 만들기 버튼 보이기
    
    * feat: useNavigate를 사용하기 위한 useNavigator 훅 추가
    
    헤더 롤링 페이퍼 만들기 버튼에 버튼 컴포넌트를 사용하게 수정
    '/post'로 이동하기 위한 useNavigator 훅 내부 handleMovePage 추가
    헤더 버튼에서 handleMovePage 호출
    버튼 컴포넌트에 handleClick prop 추가
    
    ---------
    
    Co-authored-by: mxkxx1011 <100355178+mxkxx1011@users.noreply.github.com>
    
    * feat: homepage 반응형 구현 중
    
    ---------
    
    Co-authored-by: soyoung <111185468+Ocean423@users.noreply.github.com>
    Co-authored-by: reenact11 <jaehyeon11@naver.com>
    Co-authored-by: mxkxx1011 <100355178+mxkxx1011@users.noreply.github.com>

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,26 +2,29 @@ import './HomePage.scss';
 import imageBanner01 from '../../assets/images/image_banner_01.png';
 import imageBanner02 from '../../assets/images/image_banner_02.png';
 import Button from 'components/Button';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
     <>
-      <div className='container'>
-        <div className='banner odd'>
+      <div className='main-container'>
+        <div className='banner'>
           <div className='banner-content'>
             <div className='content-point'>Point. 01</div>
             <div className='content-title'>
-              누구나 손쉽게, 온라인 롤링 페이퍼를 만들 수 있어요
+              누구나 손쉽게, 온라인 <br />
+              롤링 페이퍼를 만들 수 있어요
             </div>
             <div className='content-ex'>로그인 없이 자유롭게 만들어요.</div>
           </div>
           <img src={imageBanner01} className='banner-image' />
         </div>
-        <div className='banner even'>
+        <div className='banner'>
           <div className='banner-content'>
             <span className='content-point'>Point. 02</span>
             <div className='content-title'>
-              서로에게 이모지로 감정을 표현해보세요
+              서로에게 이모지로 감정을 <br />
+              표현해보세요
             </div>
             <div className='content-ex'>
               롤링 페이퍼에 이모지를 추가할 수 있어요.
@@ -29,9 +32,11 @@ function HomePage() {
           </div>
           <img src={imageBanner02} className='banner-image' />
         </div>
-        <Button type='primary' size='56' className='take-a-look'>
-          구경해보기
-        </Button>
+        <Link to='/list'>
+          <Button order='primary' size='56' className='take-a-look'>
+            구경해보기
+          </Button>
+        </Link>
       </div>
     </>
   );
