8075b40ed96b7da4bc3846f0f2550630f981b1d3
soyoung <111185468+Ocean423@users.noreply.github.com>
2024-06-20 14:16:39 +0900

    feat: Post페이지 반응형, 유효성검사 (#45)
    * feat: post 페이지 퍼블리싱 작업(1)
    
    * 💩: 백업
    
    * refacto: 퍼블리싱 완료
    
    * feat:클릭된 토글 값에 따라 옵션 컴포넌트 Type 변경 기능 구현
    
    * 💩test: option item값 받아오기 테스트
    
    * pull confilct solve
    
    * feat: POST 작업 진행중
    
    * feat: POST 작업 진행중
    
    * merge
    
    * 💩 branch 이동
    
    * 💩 전의 작업으로 돌아감
    
    * feat: POST TextInput 작업 완료
    
    * feat: POST Option 작업 완료
    
    * refactor: test코드 삭제, 배경 URL 변경
    
    * refactor: 코드리뷰 변경사항 적용
    
    * refactor: 코드리뷰 변경사항 적용
    
    * refactor: API_KEY 적용
    
    * feat: 반응형 구현
    
    * refactor: postpage.scss 수정
    
    * refactor: axios 코드 변경, post 객체형태로 변경
    * CallAPI.jsx 사용해서 axios post 구현하는 것으로 변경
    * post 부분 state사용, 객체 사용으로 변경
    
    * feat: input 유효성 검사 구현(outline style제외)
    
    * refactor: 유효성 검사 error-msg 폰트 사이즈 변경:
    
    ---------
    
    Co-authored-by: mxkxx1011 <100355178+mxkxx1011@users.noreply.github.com>

--- src/pages/postpage/PostPage.jsx
+++ src/pages/postpage/PostPage.jsx
@@ -6,6 +6,7 @@ import Button from 'components/Button';
 import Options from 'components/option/Options';
 import './PostPage.scss';
 import { RecipientsAPI } from 'data/CallAPI';
+import useNavigator from 'hooks/useNavigator';
 
 function PostPage() {
   // 컬러 기본값 상수로 지정
@@ -16,6 +17,7 @@ function PostPage() {
   // Option 컴포넌트의 type을 관리하는 state와 객체
   const [optionType, setOptionType] = useState('color');
   const options = ['컬러', '이미지'];
+
   // 선택된 옵션 컴포넌트 값 관리하는 state
   const [selectedOption, setSelectedOption] = useState(DEFAULT_COLOR);
 
@@ -24,59 +26,67 @@ function PostPage() {
   const handleOptionSelect = (selectedOption) => {
     setOptionType(selectedOption === '컬러' ? 'color' : 'image');
     setSelectedOption(selectedOption === '컬러' ? DEFAULT_COLOR : null);
+    // Set state for background color or image URL
+    if (selectedOption === '컬러') {
+      setBackColor(DEFAULT_COLOR);
+      setBackImageURL(null);
+    } else {
+      setBackImageURL(null);
+    }
   };
 
   // Option 컴포넌트에서 선택된 값 -> setState 지정
   const handleOptionClick = (option) => {
     setSelectedOption(option);
+    // background color, image URL 상태관리
+    if (optionType === 'color') {
+      setBackColor(option);
+      setBackImageURL(null);
+    } else if (optionType === 'image') {
+      setBackImageURL(option);
+      setBackColor(DEFAULT_COLOR);
+    }
   };
 
   /* ---- POST API 부분 ---- */
-
-  // post 요청 초기값 설정
-  const INITIAL_VALUES = {
-    team: '7-4',
-    name: '',
-    backgroundColor: DEFAULT_COLOR,
-    backgroundImageURL: null,
-  };
-  // post 전달 값 관리하는 state
-  const [values, setValues] = useState(INITIAL_VALUES);
-
-  // 이전 값과 동기화 시켜주는 함수
-  const handleChange = (name, value) => {
-    setValues((prevValues) => ({
-      ...prevValues,
-      [name]: value,
-    }));
-  };
+  const [name, setName] = useState('');
+  const [backColor, setBackColor] = useState(DEFAULT_COLOR);
+  const [backImageURL, setBackImageURL] = useState(null);
+  const handleMovePage = useNavigator();
+  const [isFocus, setIsFocus] = useState(false);
 
   // input 값 관리하는 함수
   const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    handleChange(name, value);
+    setName(e.target.value);
+  };
+
+  //포커스 아웃 되었는지 check
+  const handleBlur = () => {
+    setIsFocus(true);
   };
 
   // Form Submit 함수
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const formData = new FormData();
-    formData.append('name', values.name);
-    formData.append('team', values.team);
 
-    // optionType에 따라서 배경 색상 또는 이미지 URL을 formData에 추가
-    if (optionType === 'color') {
-      formData.append('backgroundColor', selectedOption || DEFAULT_COLOR);
-    } else if (optionType === 'image') {
-      formData.append('backgroundImageURL', selectedOption || null);
-      // backgroundImageURL이 아닌 경우에는 기본 배경색을 추가
-      formData.append('backgroundColor', DEFAULT_COLOR);
+    //값이 비어있으면 제출되지 않도록 함
+    if (!name) {
+      return;
     }
 
+    const formData = {
+      team: '7-4',
+      name: name,
+      backgroundColor: backColor,
+      backgroundImageURL: backImageURL,
+    };
+
     try {
-      RecipientsAPI('post', null, formData, null, null);
+      await RecipientsAPI('post', null, formData);
+      console.log(formData);
+      handleMovePage(`/list`);
     } catch (error) {
-      console.error('Error posting data:', error);
+      console.log(error);
     }
 
     // try {
@@ -103,39 +113,43 @@ function PostPage() {
   };
 
   return (
-    <>
-      <div className='page-container'>
-        <form className='form-container' onSubmit={handleSubmit}>
-          <label>
+    <div className='page-container'>
+      <form className='form-container' onSubmit={handleSubmit}>
+        <div className='input-container'>
+          <label htmlFor='inputName'>
             <p className='font-24-bold title-to'>To.</p>
           </label>
           <TextInputField
             name='name'
-            value={values.name}
+            value={name}
+            id='inputName'
             onChange={handleInputChange}
+            onBlur={handleBlur}
           >
             받는 사람 이름을 선택해 주세요
           </TextInputField>
-          <div className='text-container'>
-            <p className='font-24-bold title-choice'>
-              배경화면을 선택해 주세요.
-            </p>
-            <p className='font-16-regular'>
-              컬러를 선택하거나, 이미지를 선택할 수 있습니다.
-            </p>
-          </div>
-          {/* 토글버튼 컴포넌트*/}
-          <ToggleButton options={options} onOptionSelect={handleOptionSelect} />
-          {/* 옵션 컴포넌트 */}
-          <div className='option-container'>
-            <Options type={optionType} onClick={handleOptionClick} />
-          </div>
-          <Button type='submit' size='56' order='primary'>
-            생성하기
-          </Button>
-        </form>
-      </div>
-    </>
+          {/* input에 값이 없을 때 표시할 에러 메세지 */}
+          {isFocus && !name && (
+            <p className='error-message'>값을 입력해 주세요</p>
+          )}
+        </div>
+        <div className='text-container'>
+          <p className='font-24-bold title-choice'>배경화면을 선택해 주세요.</p>
+          <p className='font-16-regular'>
+            컬러를 선택하거나, 이미지를 선택할 수 있습니다.
+          </p>
+        </div>
+        {/* 토글버튼 컴포넌트*/}
+        <ToggleButton options={options} onOptionSelect={handleOptionSelect} />
+        {/* 옵션 컴포넌트 */}
+        <div className='option-container'>
+          <Options type={optionType} onClick={handleOptionClick} />
+        </div>
+        <Button type='submit' size='56' order='primary' disabled={!name}>
+          생성하기
+        </Button>
+      </form>
+    </div>
   );
 }
 
