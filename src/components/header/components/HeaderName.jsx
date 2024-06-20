34ceacdf85f69fbad5d2428abb44055a6a011b60
mxkxx <mxkxx@naver.com>
2024-06-20 23:04:14 +0900

    style: 반응형 모바일  폰트, 이미지 수정

--- src/components/header/components/HeaderName.jsx
+++ src/components/header/components/HeaderName.jsx
@@ -4,7 +4,7 @@ function HeaderName({ name, messageCount, isLoading }) {
       {isLoading ? (
         <div className='skeleton skeleton-header-name'></div>
       ) : (
-        <div className='font-28-bold header-name'>
+        <div className='header-name'>
           {messageCount == 0
             ? '첫 번째로 메시지를 작성해보세요!🥳'
             : `To. ${name}`}
