62db198b071a975bb3d941fb6d3fdbf33b4c2aa6
soyoung <ssh0040233@gmail.com>
2024-06-23 03:59:34 +0900

    fix: 이미지 셔플 버튼 제출기능 제거

--- src/components/option/RandomButton.jsx
+++ src/components/option/RandomButton.jsx
@@ -5,7 +5,7 @@ import './RandomButton.scss';
 const RandomButton = ({ onClick }) => {
   return (
     <div class='random-button-container'>
-      <button class='random-button' onClick={onClick}>
+      <button class='random-button' onClick={onClick} type='button'>
         <span>이미지 셔플</span>
       </button>
     </div>
