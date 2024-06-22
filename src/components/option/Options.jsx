4628e4fc66faac645596dbc76b95ace28f3de3c4
soyoung <ssh0040233@gmail.com>
2024-06-23 03:56:41 +0900

    feat: 옵션 컴포넌트 이미지 셔플 버튼 생성

--- src/components/option/Options.jsx
+++ src/components/option/Options.jsx
@@ -2,16 +2,16 @@ import classNames from 'classnames';
 import { useState, useEffect } from 'react';
 import Check from './Check';
 import './Options.scss';
-import { images } from './ImageBackground'; // images.js 파일에서 이미지 배열 가져오기
+import { images } from './ImageBackground';
+import RandomButton from './RandomButton';
 
-//랜덤 옵션 이미지 지정
+// 랜덤 옵션 이미지 지정
 const shuffleArray = (array) => {
   let currentIndex = array.length,
     randomIndex;
   while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
-    //랜덤 아이템 배열 생성
     [array[currentIndex], array[randomIndex]] = [
       array[randomIndex],
       array[currentIndex],
@@ -48,7 +48,14 @@ function Options({ type = 'image', onClick, onFirstOptionSelect }) {
     onClick(OptionArray[idx]); // 선택된 옵션 값 전달
   };
 
-  // 토글 변환 시 작동
+  // 이미지 랜덤 변경 함수
+  const handleImageShuffle = () => {
+    const shuffled = shuffleArray([...images]).slice(0, 4);
+    setShuffledImages(shuffled);
+    onFirstOptionSelect(shuffled[0]);
+    setIsSelected(0);
+  };
+
   useEffect(() => {
     if (type === 'image') {
       const shuffled = shuffleArray([...images]).slice(0, 4);
@@ -57,21 +64,28 @@ function Options({ type = 'image', onClick, onFirstOptionSelect }) {
     } else {
       onFirstOptionSelect(colors[0]);
     }
-    setIsSelected(0);
+    setIsSelected(0); // 첫 번째 이미지를 선택 상태로 설정
   }, [type]);
 
   return (
-    <div className='options'>
-      {OptionArray.map((option, idx) => (
-        <Option
-          key={idx}
-          idx={idx}
-          isSelected={isSelected}
-          color={type === 'color' ? option : null}
-          handleClick={() => handleClick(idx)}
-          image={type === 'image' ? option : null}
-        />
-      ))}
+    <div>
+      {type === 'image' && (
+        <RandomButton onClick={handleImageShuffle}>
+          랜덤 이미지 설정
+        </RandomButton>
+      )}
+      <div className='options'>
+        {OptionArray.map((option, idx) => (
+          <Option
+            key={idx}
+            idx={idx}
+            isSelected={isSelected}
+            color={type === 'color' ? option : null}
+            handleClick={() => handleClick(idx)}
+            image={type === 'image' ? option : null}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/option/RandomButton.jsx
@@ -0,0 +1,15 @@
+// RandomizeButton.jsx
+import React from 'react';
+import './RandomButton.scss';
+
+const RandomButton = ({ onClick }) => {
+  return (
+    <div class='random-button-container'>
+      <button class='random-button' onClick={onClick}>
+        <span>이미지 셔플</span>
+      </button>
+    </div>
+  );
+};
+
+export default RandomButton;
